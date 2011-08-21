0d492b34beb8afa1de67511533824f89e7a79e2d
Andrew Drake <adrake@adrake.org>
2011-08-21 07:50:47 -0400

    Add cation for performance regression fuzzing.

--- README.txt
+++ README.txt
@@ -3,12 +3,18 @@ utilities that the author happened to have installed). The shell scripts
 extensively use bash extensions (some requiring as new as bash >= 4.2), so a
 modern distro is likely required.
 
-To use anion, read the "config options" of anion.sh, produce a configuration
-file with appropriate paths, and run ./multidriver.sh ./anion.sh <configfile>
-<n> to run n instances of the fuzzer in parallel. Results will show up in
-results-<configname>, organized by failure mode and signature hash.
+To use anion, read the "config options" section of anion.sh, produce a
+configuration file with appropriate paths, and run ./multidriver.sh ./anion.sh
+<configfile> <n> to run n instances of the fuzzer in parallel. Results will show
+up in results-<configname>, organized by failure mode and signature hash.
 
-The test generator API is as simple as it gets. Just produce valid JavaScript on
-standard out (the fuzz driver will choke if it gets invalid testcases). There's
-a simple but surprisingly effective general purpose generator included, and
-writing special case generators is both productive and easy.
+Cation is a little more involved. Add "CATION:<TAG>\n" printfs to the code
+wherever you encounter something you wish to count (e.g. when a move instruction
+is emitted). Configuration works the same way as anion, except there are two
+sets of parameters to the JS engine and you need to identify <TAG>.
+
+The test generator API is shared between anion and cation and is as simple as it
+gets. Just produce valid JavaScript on standard out (the fuzz driver will choke
+if it gets invalid testcases). There's a simple but surprisingly effective
+general purpose generator included, and writing special case generators is both
+productive and easy.
---
+++ cation.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <config>" >&2
+    exit 1
+fi
+
+# Settings
+function setConfigName() { declare -g -r cConfigName="$1"; }
+function setGenerator() { declare -g -r cGenerator="$1"; }
+function setGeneratorParams() { declare -g -r cGeneratorParams="$@"; }
+function setIonBinary() { declare -g -r cIonBinary="$1"; }
+function setIonParamsA() { declare -g -r cIonParamsA="$@"; }
+function setIonParamsB() { declare -g -r cIonParamsB="$@"; }
+function setTracer() { declare -g -r cTracer="$1"; }
+. "$1"
+
+# Parameters
+declare -r pTempDir="/tmp"
+declare -r pResultDir="results${cConfigName:+-}${cConfigName:-}"
+declare -r pHash="md5sum"
+declare -i -r pHashLength=32
+
+declare -r pGenerator="${cGenerator:?}"
+declare -r pGeneratorParams="${cGeneratorParams:-}"
+
+declare -r pIonBinary="${cIonBinary:?}"
+declare -r pIonParamsA="${cIonParamsA:-}"
+declare -r pIonParamsB="${cIonParamsB:-}"
+declare -r pTimeout="1"
+declare -r pTracer="${cTracer:?}"
+
+# Global state
+declare gTestSource
+declare gTestSourceHash
+
+# Functions
+function die() {
+    echo "$1" >&2
+    exit 1
+}
+
+function spam() {
+    echo -n "$1"
+}
+
+function tempExt() {
+    mktemp "${pTempDir}/XXXXXXXX.${1}"
+}
+
+function generateTest() {
+    gTestSource="$(tempExt js)"
+    "$pGenerator" $pGeneratorParams > "$gTestSource" || return $?
+
+    local hashLine="$("$pHash" "$gTestSource")"
+    gTestSourceHash="${hashLine:0:pHashLength}"
+    return 0
+}
+
+function runTest() {
+    local params="$1"
+    (ulimit -t "$pTimeout"; exec 2>/dev/null; "${pIonBinary}" $params "${gTestSource}" | egrep "^CATION:${pTracer}\$" | wc -l)
+}
+
+function cleanup() {
+    [[ -f "$gTestSource" ]] && rm -f "$gTestSource"
+    return 0
+}
+
+function bail() {
+    cleanup
+    exit 0
+}
+
+# Make sure we actually die on Ctrl-C/Ctrl-\
+trap bail INT TERM
+
+mkdir -p "${pResultDir}" || die "Failed to create output directory"
+while true; do
+    generateTest || die "Failed to generate test"
+
+    resultsA="$(runTest "$pIonParamsA")"
+    [[ $? -ne 0 ]] && continue
+    resultsB="$(runTest "$pIonParamsB")"
+    [[ $? -ne 0 ]] && continue
+
+    if [[ "$resultsA" -lt "$resultsB" ]]; then
+	# Regression found!
+	if [[ "$resultsA" -eq 0 ]]; then
+	    echo -e "Infty\t${gTestSourceHash}" >> "${pResultDir}/scorecard"
+	else
+	    regressPercent="$(dc -e "4k ${resultsB} ${resultsA} - ${resultsA} / 100 * 2k 1 / p")"
+	    echo -e "${regressPercent}\t${gTestSourceHash}" >> "${pResultDir}/scorecard"
+	fi
+	mv "${gTestSource}" "${pResultDir}/${gTestSourceHash}.js"
+	spam "-"
+    elif [[ "$resultsA" -gt "$resultsB" ]]; then
+	spam "+"
+    fi
+
+    cleanup
+done
